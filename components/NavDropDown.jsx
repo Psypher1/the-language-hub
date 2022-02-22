a3ca39132ba5a8d20466f30d4dc9e2cdcd1a3adc
Dante <jamesmidzi@gmail.com>
2022-02-22 23:35:56 +0200

    fixed dropdown issue on mobile

--- components/NavDropDown.jsx
+++ components/NavDropDown.jsx
@@ -10,48 +10,74 @@ import {
   MenuItem,
   useDisclosure,
   Portal,
+  Button,
   IconButton,
 } from "@chakra-ui/react";
-
-// import { XIcon, MenuIcon } from "@heroicons/react/outline";
 
 export default function NavDropDown() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Menu isOpen={isOpen}>
+    <Menu>
       <MenuButton
-        as={IconButton}
+        as={Button}
         _hover={{ bg: "blue.100" }}
         _focus={{ bg: "blue.100" }}
         _active={{ bg: "blue.100" }}
-        px={8}
+        px={6}
         py={4}
-        fontSize={{ fontSize: "1.25rem" }}
-        transition="all 0.2s"
-        onMouseEnter={onOpen}
-        onMouseLeave={onClose}
       >
-        <i className="fa-solid fa-chevron-down text-xl transition duration-300 hover:bg-sky-200 focus:bg-sky-200"></i>
+        <i className="fas fa-chevron-down text-xl font-semibold"></i>
       </MenuButton>
 
-      <Portal>
-        <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
-          {allLangLogos.map((logo, index) => {
-            const learPath = logo.langPath.toLowerCase();
-            return (
-              <MenuItem key={index} _hover={{ bg: "blue.100" }}>
-                <Link key={index} href={`/${learPath}/basics`} passHref>
-                  <a className="mb-2 flex cursor-pointer items-center space-x-2">
-                    <img src={logo.logoURL} className="h-5 w-5" />
-                    <span>{logo.langPath}</span>
-                  </a>
-                </Link>
-              </MenuItem>
-            );
-          })}
-        </MenuList>
-      </Portal>
+      <MenuList backgroundColor="blue.50">
+        {allLangLogos.map((logo, index) => {
+          const learPath = logo.langPath.toLowerCase();
+          return (
+            <MenuItem key={index} _hover={{ bg: "blue.100" }}>
+              <Link key={index} href={`/${learPath}/basics`} passHref>
+                <a className="mb-2 flex cursor-pointer items-center space-x-2">
+                  <img src={logo.logoURL} className="h-5 w-5" />
+                  <span>{logo.langPath}</span>
+                </a>
+              </Link>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
     </Menu>
+
+    // <Menu isOpen={isOpen}>
+    //   <MenuButton
+    //     as={Button}
+    //     _hover={{ bg: "blue.100" }}
+    //     _focus={{ bg: "blue.100" }}
+    //     _active={{ bg: "blue.100" }}
+    //     px={8}
+    //     py={4}
+    //     fontSize={{ fontSize: "1.25rem" }}
+    //     transition="all 0.2s"
+    //   >
+    //     Path
+    //   </MenuButton>
+
+    //   <Portal>
+    //     <MenuList>
+    //       {allLangLogos.map((logo, index) => {
+    //         const learPath = logo.langPath.toLowerCase();
+    //         return (
+    //           <MenuItem key={index} _hover={{ bg: "blue.100" }}>
+    //             <Link key={index} href={`/${learPath}/basics`} passHref>
+    //               <a className="mb-2 flex cursor-pointer items-center space-x-2">
+    //                 <img src={logo.logoURL} className="h-5 w-5" />
+    //                 <span>{logo.langPath}</span>
+    //               </a>
+    //             </Link>
+    //           </MenuItem>
+    //         );
+    //       })}
+    //     </MenuList>
+    //   </Portal>
+    // </Menu>
   );
 }
