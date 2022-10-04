ef045a2961994133f94a781989c0b8c4e6bc1f72
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-10-04 13:23:21 +0200

    refactored language path navigation

--- components/LangPathNav.jsx
+++ components/LangPathNav.jsx
@@ -6,46 +6,26 @@ export default function LangPathNav({ langPath }) {
     "px-5 py-2 hover:bg-sky-600 hover:font-semibold rounded site-transition";
   const isActive = "bg-sky-900 font-semibold";
 
-  // Language Paths to be matched
-  const frenchPath = "french";
-  const russianPath = "russian";
-  const shonaPath = "shona";
-  const swahiliPath = "swahili";
-  const ndebelePath = "ndebele";
+  const languages = [
+    { name: "French", path: "french" },
+    { name: "Japanese", path: "japanese" },
+    { name: "Ndebele", path: "ndebele" },
+    { name: "Russian", path: "russian" },
+    { name: "Shona", path: "shona" },
+    { name: "Swahili", path: "swahili" },
+  ];
 
   return (
     <div className="flex items-center justify-center space-x-2 bg-sky-700 px-8 py-3 text-base text-sky-100">
-      <Link href={`/french/basics`}>
-        <a className={`${menuItem} ${langPath === frenchPath ? isActive : ""}`}>
-          French
-        </a>
-      </Link>
-      <Link href={`/ndebele/basics`}>
-        <a
-          className={`${menuItem} ${langPath === ndebelePath ? isActive : ""}`}
-        >
-          Ndebele
-        </a>
-      </Link>
-      <Link href={`/russian/basics`}>
-        <a
-          className={`${menuItem} ${langPath === russianPath ? isActive : ""}`}
-        >
-          Russian
-        </a>
-      </Link>
-      <Link href={`/shona/basics`}>
-        <a className={`${menuItem} ${langPath === shonaPath ? isActive : ""}`}>
-          Shona
-        </a>
-      </Link>
-      <Link href={`/swahili/basics`}>
-        <a
-          className={`${menuItem} ${langPath === swahiliPath ? isActive : ""}`}
-        >
-          Swahili
-        </a>
-      </Link>
+      {languages.map((lang) => (
+        <Link href={`/${lang.path}/basics`}>
+          <a
+            className={`${menuItem} ${langPath === lang.path ? isActive : ""}`}
+          >
+            {lang.name}
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
