f3c77ef8f38c3e866075fe76a275025397d87039
James 'Dante' Midzi <jamesmidzi@gmail.com>
2023-01-20 13:55:27 +0200

    chore: returned missing _app.js file

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import "../styles/globals.css";
+
+// SEO Imports
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
+
+// Layout Imports
+import Wrapper from "@layouts/Wrapper";
+
+// Chakra UI
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<ChakraProvider>
+				<Wrapper>
+					{/* <Header /> */}
+					<section className="min-h-screen">
+						<DefaultSeo {...SEO} />
+						<Component {...pageProps} />
+					</section>
+					{/* <Footer /> */}
+				</Wrapper>
+			</ChakraProvider>
+		</>
+	);
+}
+
+export default MyApp;
