a05e9c75d6695f099abc0c48e17eb7461ebe155c
James 'Dante' Midzi <jamesmidzi@gmail.com>
2023-02-23 22:45:16 +0200

    added og image

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,29 +3,29 @@ const description = "Getting you to what's important as easily as possible";
 const URL = "https://thelanguagehub.netlify.app/";
 
 const SEO = {
-  title,
-  description,
-  canonical: URL,
-  openGraph: {
-    type: "website",
-    locale: "en_IE",
-    url: URL,
-    site_name: title,
-    description,
-    images: [
-      {
-        url: "https://thelanguagehub/og-image.png",
-        width: 800,
-        height: 600,
-        alt: "Og Image Alt",
-      },
-    ],
-  },
-  twitter: {
-    handle: "@Psypher1",
-    site: "@Psypher1",
-    cardType: "summary_large_image",
-  },
+	title,
+	description,
+	canonical: URL,
+	openGraph: {
+		type: "website",
+		locale: "en_IE",
+		url: URL,
+		site_name: title,
+		description,
+		images: [
+			{
+				url: "https://thelanguagehub/og-image.jpg",
+				width: 1200,
+				height: 550,
+				alt: "The Language Hub OG Image",
+			},
+		],
+	},
+	twitter: {
+		handle: "@Psypher1",
+		site: "@Psypher1",
+		cardType: "summary_large_image",
+	},
 };
 
 export default SEO;
