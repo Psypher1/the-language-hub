d94f2d7dd5bc44f5c65650dc2670b323383e8c81
Dante <jamesmidzi@gmail.com>
2026-07-14 10:14:52 +0200

    feat: polish up docs stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,43 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import starlight from "@astrojs/starlight";
 
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [
+    tailwind(),
+    icon(),
+    starlight({
+      title: "The Language Hub",
+      description: "Your language reference book",
+      customCss: ["src/styles/custom.css"],
+      logo: {
+        light: "public/logo-blue.svg",
+        dark: "public/logo-white.svg",
+      },
+      disable404Route: true,
+      sidebar: [
+        {
+          label: "Documentation",
+          items: [
+            "docs",
+            "docs/getting-started",
+            {
+              label: "Contributing",
+              autogenerate: { directory: "docs/contributing" },
+            },
+            "docs/about",
+          ],
+        },
+      ],
+      components: {
+        ThemeProvider: "./src/components/starlight-theme-provider.astro",
+      },
+    }),
+    mdx(),
+  ],
 });
